4a6c44de3d0dedefe5720155681c460171ddd702
souravrane <sourav1121997@gmail.com>
2021-06-04 08:24:24 +0530

    base page ready

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { Canvas } from "@react-three/fiber";
 
-const Box = () => {
-  return <mesh></mesh>;
-};
-
 export default function Home() {
-  return <Canvas></Canvas>;
+  return (
+    <div style={{ textAlign: "center", position: "relative", top: "50vh" }}>
+      3D Models
+    </div>
+  );
 }
